0aaa88f0378d9d0c64c552364545f760179aca33
ahoyosid <ahoyosidrobo@gmail.com>
2016-03-19 17:41:34 +0100

    minor changes

--- script_hrf_peak.py
+++ script_hrf_peak.py
@@ -32,18 +32,18 @@ hrf_ushoot = 16.
 # GP parameters
 time_offset = 10
 gamma = 3.0
-fmin_max_iter = 10
+fmin_max_iter = 20
 n_restarts_optimizer = 0
-n_iter = 1
+n_iter = 3
 normalize_y = False
 optimize = True
 zeros_extremes = True
 
-range_peak = np.arange(5, 6)
+range_peak = np.arange(2, 8)
 
 
-for sigma_noise in np.array([.1]):
-# for sigma_noise in np.array([2., 1., 0.5, 0.1, 0.01]):
+# for sigma_noise in np.array([5., 2., 1., 0.5, 0.1, 0.01]):
+# for sigma_noise in np.array([5]):
 
     plt.figure(figsize=(12, 8))
     i = 0
@@ -67,23 +67,6 @@ for sigma_noise in np.array([.1]):
                                         event_types=event_types, period_cut=64,
                                         time_offset=10, modulation=None, seed=seed)
 
-
-        # Mean function of GP set to a certain HRF model
-        hrf_model = 'glover'
-        hrf_0 = _get_hrf_model(hrf_model, hrf_length=hrf_length + dt, dt=dt,
-                               normalize=True)
-        f_hrf = interp1d(x_0, hrf_0)
-        f_hrf = None
-
-        # Estimation with 1 hrf
-        gp = SuperDuperGP(hrf_length=hrf_length, t_r=t_r, oversampling=1./dt, modulation=modulation,
-                          gamma=gamma, fmin_max_iter=fmin_max_iter,
-                          sigma_noise=sigma_noise, time_offset=time_offset,
-                          n_iter=n_iter, normalize_y=normalize_y, verbose=True,
-                          optimize=optimize,
-                          n_restarts_optimizer=n_restarts_optimizer,
-                          zeros_extremes=zeros_extremes, f_mean=f_hrf)
-
         design = design[event_types].values  # forget about drifts for the moment
         beta = rng.randn(len(event_types))
         ys = design.dot(beta)
@@ -91,7 +74,7 @@ for sigma_noise in np.array([.1]):
         scale_factor = np.linalg.norm(ys) / np.linalg.norm(noise)
         ys_acquired = ys + noise * scale_factor * sigma_noise
 
-        snr = 20 * (np.log10(np.linalg.norm(ys_acquired) / sigma_noise))
+        snr = 20 * (np.log10(np.linalg.norm(ys) / np.linalg.norm(ys - ys_acquired)))
         print 'SNR = ', snr, ' dB'
 
         # Estimation with 1 hrf. Uses glover as mean GP
@@ -99,6 +82,8 @@ for sigma_noise in np.array([.1]):
         hrf_0 = _get_hrf_model(hrf_model, hrf_length=hrf_length + dt,
                                dt=dt, normalize=True)
         f_hrf = interp1d(x_0, hrf_0)
+        f_hrf = None
+
         gp = SuperDuperGP(hrf_length=hrf_length, t_r=t_r, oversampling=1./dt, gamma=gamma,
                     modulation=modulation, fmin_max_iter=fmin_max_iter, sigma_noise=sigma_noise,
                     time_offset=time_offset, n_iter=n_iter, normalize_y=normalize_y, verbose=True,
