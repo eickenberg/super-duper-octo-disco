981d133d97fa3b66bad4d5bb44183fa14b9f3349
ainafp <ainafp@gmail.com>
2016-03-24 18:02:56 +0100

    Analysis of AINSI_012 on held-out data

--- real_data_analysis_ainsi012.py
+++ real_data_analysis_ainsi012.py
@@ -14,17 +14,21 @@ from scipy.interpolate import interp1d
 folder = 'data_ainsi012'
 folder0 = 'data_example'
 
-#studies = [ 'audioD_mean_timeseries', 'audioG_mean_timeseries',
-#            'visualD_mean_timeseries','visualG_mean_timeseries',
-#            'motorD_mean_timeseries', 'motorG_mean_timeseries' ]
+"""studies = [ 'audioD_mean_timeseries', 'audioG_mean_timeseries',
+            'visualD_mean_timeseries','visualG_mean_timeseries',
+            'motorD_mean_timeseries', 'motorG_mean_timeseries' ]
+"""
 
-"""studies = [ 'audioD_voxel193_mean1v', 'audioG_voxel166_mean1v',
+n_vox = 1
+studies = [ 'audioD_voxel193_mean1v', 'audioG_voxel166_mean1v',
             'visualD_voxel2_mean1v', 'visualG_voxel111_mean1v',
             'motorD_voxel481_mean1v', 'motorG_voxel277_mean1v' ]
 """
+n_vox = 6
 studies = [ 'audioD_voxel193_mean6v', 'audioG_voxel166_mean5v',
             'visualD_voxel2_mean5v', 'visualG_voxel111_mean4v',
             'motorD_voxel481_mean5v', 'motorG_voxel277_mean5v' ]
+"""
 
 # Define HRF of mean GP
 hrf_length = 32
@@ -40,12 +44,14 @@ f_hrf = interp1d(x_0, hrf_0)
 if True:
     for study in studies:
         voxel_fn = op.join(folder, study + '.npy')
+        voxel_r2_fn = op.join(folder, study + '_r2.npy')
         # Paradigm file
         paradigm_fn = op.join(folder0, 'onsets.csv')
         ########################################################################
         # Load data and parameters
         t_r = 3.
         ys = np.load(voxel_fn)
+        ysr2 = np.load(voxel_r2_fn)
         n_scans = ys.shape[0]
 
         # Create design matrix
@@ -64,7 +70,7 @@ if True:
         optimize = False
         zeros_extremes = True
 
-        # Estimation
+        # Estimation HRF on 1 run
         gp = SuperDuperGP(hrf_length=hrf_length, t_r=t_r, oversampling=1./dt,
                           gamma=gamma, modulation=modulation,
                           fmin_max_iter=fmin_max_iter, sigma_noise=1.0,
@@ -74,44 +80,61 @@ if True:
                           n_restarts_optimizer=n_restarts_optimizer,
                           zeros_extremes=zeros_extremes, f_mean=f_hrf)
 
-        print ys.shape
-        print paradigm.shape
         (hx, hy, hrf_var, resid_norm_sq, sigma_sq_resid) = gp.fit(ys, paradigm)
-
         print 'residual norm square = ', resid_norm_sq
 
-        # Testing with a GLM
+        # make the design matrix using estimated hrf
+        f_hrf_est = interp1d(hx, hy)
+
+        from data_generator import make_design_matrix_hrf
+        dm2 = make_design_matrix_hrf(frametimes, paradigm=paradigm,
+        	hrf_length=hrf_length, t_r=t_r, time_offset=time_offset,
+        	f_hrf=f_hrf_est)
+
+
+        # Predict using the betas with GP
+        ys_pred, matrix, betas, resid = gp.predict(ysr2, paradigm)
+
+
+        # Prepare data for GLM
         mask_img = nb.Nifti1Image(np.ones((2, 2, 2)), affine=np.eye(4))
         masker = NiftiMasker(mask_img=mask_img)
         masker.fit()
-        ys2 = np.ones((2, 2, 2, ys.shape[0])) * ys[np.newaxis, np.newaxis, np.newaxis, :]
+        ys2 = np.ones((2, 2, 2, ys.shape[0])) * ysr2[np.newaxis, np.newaxis, np.newaxis, :]
         niimgs = nb.Nifti1Image(ys2, affine=np.eye(4))
+
+        # Re-estimate with a GLM on run 2 with dm
         glm = FirstLevelGLM(mask=mask_img, t_r=t_r, standardize=True, noise_model='ols')
         glm.fit(niimgs, dm)
-        norm_resid = (np.linalg.norm(glm.results_[0][0].resid, axis=0)**2).mean()
-        ys_pred_glm = glm.results_[0][0].predicted[:, 0]
+        norm_resid1 = (np.linalg.norm(glm.results_[0][0].resid, axis=0)**2).mean()
+        ys_pred_glm1 = glm.results_[0][0].predicted[:, 0]
 
-        # Predict GP
-        # XXX: Do we need to predict for GLM???
-        ys_pred, matrix, betas, resid = gp.predict(ys, paradigm)
+        # Re-estimate with a GLM on run 2 with dm2
+        glm.fit(niimgs, dm2)
+        norm_resid2 = (np.linalg.norm(glm.results_[0][0].resid, axis=0)**2).mean()
+        ys_pred_glm2 = glm.results_[0][0].predicted[:, 0]
 
-        corr_gp = np.corrcoef(ys_pred, ys)[1, 0]
-        corr_glm = np.corrcoef(ys_pred_glm, ys)[1, 0]
+        corr_gp = np.corrcoef(ys_pred, ysr2)[1, 0]
+        corr_glm1 = np.corrcoef(ys_pred_glm1, ysr2)[1, 0]
+        corr_glm2 = np.corrcoef(ys_pred_glm2, ysr2)[1, 0]
 
-        print "corr glm: %s, corr gp: %s" % (corr_glm, corr_gp)
+        print "corr glm1: %s, corr glm2: %s, corr gp: %s" % (corr_glm1, corr_glm2, corr_gp)
 
         data = {}
         data['ys'] = ys
         data['study'] = study
-        data['ys_pred_glm'] = ys_pred_glm
+        data['ys_pred_glm1'] = ys_pred_glm1
+        data['ys_pred_glm2'] = ys_pred_glm2
         data['ys_pred_gp'] = ys_pred
-        data['corr_glm'] = corr_glm
+        data['corr_glm1'] = corr_glm1
+        data['corr_glm2'] = corr_glm2
         data['corr_gp'] = corr_gp
         data['hy'] = hy
         data['hx'] = hx
         data['hrf_var'] = hrf_var
         data['norm_resid_gp'] = resid_norm_sq
-        data['norm_resid_glm'] = norm_resid
+        data['norm_resid_glm1'] = norm_resid1
+        data['norm_resid_glm2'] = norm_resid2
 
         np.save(op.join(folder, study + '_data.npy'), data)
 
@@ -128,22 +151,26 @@ if True:
         data = np.load(op.join(folder, study + '_data.npy')).item()
 
         ys = data['ys']
-        ys_pred_glm = data['ys_pred_glm']
+        ys_pred_glm = data['ys_pred_glm1']
+        ys_pred_glm = data['ys_pred_glm2']
         ys_pred = data['ys_pred_gp']
-        corr_glm = data['corr_glm']
+        corr_glm1 = data['corr_glm1']
+        corr_glm2 = data['corr_glm2']
         corr_gp = data['corr_gp']
         hy = data['hy']
         hx = data['hx']
         hrf_var = data['hrf_var']
 
-        print "STUDY: %s | corr glm: %.2f | corr gp: %.2f" % (study, corr_glm,
-                                                              corr_gp)
+        print "STUDY: %s | corr glm: %.2f | corr glm_h: %.2f | corr gp: %.2f" % \
+        				(study, corr_glm1, corr_glm2, corr_gp)
 
         hy *= np.sign(hy[np.argmax(np.abs(hy))]) / np.abs(hy).max()
         hrf_0 /= hrf_0.max()
 
         # Plot HRF
         plt.figure(1)
+        if regions[istudy]=='visual right':
+        	hy = - hy
         plt.fill_between(hx, hy - 1.96 * np.sqrt(hrf_var),
                         hy + 1.96 * np.sqrt(hrf_var),
                         color=colors[istudy], alpha=0.1)
@@ -151,10 +178,11 @@ if True:
         plt.plot(hx, hy, color=colors[istudy], label='HRF ' + regions[istudy])
 
         plt.hold('on')
+
     plt.plot(x_0, hrf_0, color='k', label='glover HRF')
     plt.axis('tight')
     plt.legend()
-    fname = op.join(folder, "hrf_regions_joint_small_1voxels_gamma" + str(gamma))
+    fname = op.join(folder, "hrf_regions_joint_small_%dvoxels_gamma%d" % (n_vox, gamma))
     #fname = op.join(folder, "hrf_regions_joint_small_20voxels")
     plt.savefig(fname + ".png")
     plt.savefig(fname + ".pdf")
