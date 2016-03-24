63a9c5eb3bf844d3594416ba5a1b137e1ab75905
ahoyosid <ahoyosidrobo@gmail.com>
2016-03-24 23:29:52 +0100

    preparing code to plot the variance

--- plots_prni.py
+++ plots_prni.py
@@ -58,26 +58,52 @@ test_proj_resid_gp = f['test_projection_residuals_gp']
 test_proj_resid_gp_zero = f['test_projection_residuals_gp_zero']
 
 noise_levels_index = [1, 2, 3]
-fig, axx = plt.subplots(nrows=2, ncols=3, figsize=(15, 7), sharey=True,
-                        sharex=True)
+fig, axx = plt.subplots(nrows=2, ncols=3, figsize=(15, 7),
+                        sharey=True,
+                        # sharex=True
+                        )
+
+# 1- residnorm / signal norm
+
+# note: the mean(axis=2) is the folds
+# mean(axis=-1) for different noise initializations
+signal_norm = f['test_norm_squared']
+
+score_test_pred_resid_glm = 1. - test_pred_resid_glm / signal_norm
+score_test_pred_resid_gp = 1. - test_pred_resid_gp / signal_norm
+score_test_pred_resid_gp_zero = 1. - test_pred_resid_gp_zero / signal_norm
+
+# score_test_proj_resid_glm = 1. - test_proj_resid_glm / signal_norm
+# score_test_proj_resid_gp = 1. - test_proj_resid_gp / signal_norm
+# score_test_proj_resid_gp_zero = 1. - test_proj_resid_gp_zero / signal_norm
+
+score_pred_glm = score_test_pred_resid_glm.mean(axis=-1).mean(axis=2)
+score_pred_gp = score_test_pred_resid_gp.mean(axis=-1).mean(axis=2)
+score_pred_gp_zero = score_test_pred_resid_gp_zero.mean(axis=-1).mean(axis=2)
+
+# score_proj_glm = score_test_proj_resid_glm.mean(axis=-1).mean(axis=2)
+# score_proj_gp = score_test_proj_resid_gp.mean(axis=-1).mean(axis=2)
+# score_proj_gp_zero = score_test_proj_resid_gp_zero.mean(axis=-1).mean(axis=2)
 
 formatter = FuncFormatter(add_s)
 for col, j in enumerate(noise_levels_index):
-    axx[0, col].plot(test_pred_resid_glm.mean(axis=-1).mean(axis=2)[..., j], 'r')
-    axx[0, col].plot(test_pred_resid_gp.mean(axis=-1).mean(axis=2)[..., j], 'g')
-    axx[0, col].plot(test_pred_resid_gp_zero.mean(axis=-1).mean(axis=2)[:, 0, ..., j], 'b')
+    axx[0, col].plot(score_pred_glm[..., j], 'r', linewidth=2)
+    axx[0, col].plot(score_pred_gp[..., j], 'g')
+    axx[0, col].plot(score_pred_gp_zero[:, 0, ..., j], 'b')
     axx[0, col].set_title('noise = %s' % j)
 
-    axx[1, col].plot(test_proj_resid_glm.mean(axis=-1).mean(axis=2)[..., j], 'r')
-    axx[1, col].plot(test_proj_resid_gp.mean(axis=-1).mean(axis=2)[..., j], 'g')
-    axx[1, col].plot(test_proj_resid_gp_zero.mean(axis=-1).mean(axis=2)[:, 0, ..., j], 'b')
+    # axx[1, col].plot(score_proj_glm[..., j], 'r')
+    # axx[1, col].plot(score_proj_gp[..., j], 'g')
+    # axx[1, col].plot(score_proj_gp_zero[:, 0, ..., j], 'b')
 
-    axx[0, col].set_title('noise = %s' % j)
-    axx[1, col].set_ylim([0, 0.002])
-    axx[0, col].set_ylim([0, 0.002])
-    plt.setp(axx[1, col], xticks=range(hrf_peak_location.shape[0]),
-             xticklabels=hrf_peak_location)
-    axx[1, col].xaxis.set_major_formatter(formatter)
+    axx[0, col].set_title('noise = %s' % noise_levels[j])
+    axx[1, col].set_ylim([0, 1.])
+    # axx[0, col].set_ylim([1e-6, 1e-3])
+    # plt.setp(axx[1, col], xticks=range(hrf_peak_location.shape[0]),
+    #          xticklabels=hrf_peak_location)
+    # axx[1, col].xaxis.set_major_formatter(formatter)
+    # axx[1, col].set_xlabel('HRF peak location \n for generation')
+    # axx[0, col].set_yscale('log')
 
 axx[0, 0].set_ylabel('Residual error')
 axx[1, 0].set_ylabel('Residual error')
@@ -87,6 +113,9 @@ l_gp = Rectangle((0, 0), 1, 1, fc='g', alpha=0.8, edgecolor='.8')
 l_gp_zero = Rectangle((0, 0), 1, 1, fc='b', alpha=0.8, edgecolor='.8')
 
 lgd = plt.legend([l_glm, l_gp, l_gp_zero], ['GLM', 'GP', 'zero-mean GP'],
-                 ncol=3, loc=(-2., -1))
+                 ncol=3, loc=(-2.5, -1))
 
-fig.savefig('super_duper_diagram.pdf', bbox_inches='tight')
+
+fig.savefig('images/super_duper_diagram.pdf', bbox_inches='tight')
+fig.savefig('images/super_duper_diagram.png', bbox_inches='tight')
+fig.savefig('images/super_duper_diagram.svg', bbox_inches='tight')
